bb97087587c4924693357563cd28b97bd5e369ce
wangbei98 <beiwang121@163.com>
2020-06-30 15:27:30 +0800

    完成 translate

--- apis/translateapi.py
+++ apis/translateapi.py
@@ -6,10 +6,29 @@ from flask_restful import Api,Resource,fields,marshal_with,marshal_with_field,re
 from models import Question,Answer
 from extensions import db
 import re
+import json
 
 def replace_word(matched):
 	word = matched.group('word').lower()
-	return '[' + word + ']'
+	after = word
+	try:
+		question = Question.query.filter_by(word = word).first()
+	except:
+		return '[' + after + ']'
+	if not question:
+		return '[' + after + ']'
+	answers = question.answers
+	if len(answers) == 0:
+		return '[' + after + ']'
+	after = answers[0].meaning
+	max_like = answers[0].like
+	for answer in answers:
+		if answer.like > max_like:
+			after = answer.meaning
+			max_like = answer.like
+	return '[' + after + ']'
+
+	
 
 class TranslateAPI(Resource):
 	def get(self):
